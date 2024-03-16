9ff597f2bb1d4ebc48fba425b477939d8af1aaac
Ralph John Galindo <ralph@burstsms.com>
2024-03-16 23:28:38 +0800

    feat: Favicon

--- app.vue
+++ app.vue
@@ -56,6 +56,10 @@ useHead(() => {
       { rel: 'preload', href: '/envelope-outside-gold.png', as: 'image' },
       { rel: 'preload', href: '/envelope-gold.png', as: 'image' },
       { rel: 'preload', href: '/letter_blank.png', as: 'image' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' }
     ]
   };
 });
