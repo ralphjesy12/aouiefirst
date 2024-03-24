f582e6717ed6f4b8872566d8fb0efb07f5a172dd
Ralph John Galindo <ralph@burstsms.com>
2024-03-24 21:07:48 +0800

    feat: Update Form Link

--- components/RsvpForm.vue
+++ components/RsvpForm.vue
@@ -3,7 +3,7 @@
     <div class="letter-title">You are invited!</div>
     <p class="letter-description">We reserved seats for you and your family. <br>Please confirm your attendance below.</p>
     <p style="margin:50px 0px 0 0px;text-align:center;">
-      <a href="https://www.addevent.com/event/nH20655492" title="RSVP" target="_blank" style="display:inline;">
+      <a href="https://form.jotform.com/240833295186462" title="RSVP" target="_blank" style="display:inline;">
         <img src="https://buttons.addevent.com/rsvp-813b0a-r48-icotxt-s40.png" alt="RSVP" height="40" border="0" style="height:40px;display:inline;" />
       </a>
     </p>
